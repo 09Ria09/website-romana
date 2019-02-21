15b2a7a166bba0053d74a7bf1783e5524a70afff
Rares Iordan <raresiordan09@gmail.com>
2019-02-21 19:19:56 +0200

    Now it looks like a real website. TODO: Polish the Design and find suitable backgrounds.

--- js/global.js
+++ js/global.js
@@ -11,19 +11,34 @@ function animateCss(element, animationName, callback) {
 }
 
 function changePage(page) {
-    $("body").css("overflow-x", "hidden");
+    $("html").css("overflow", "hidden");
+    $("html")[0].offsetHeight; // flushes CSS buffer
 
-    animateCss($("body"), "slideOutUp", function () {
-        $("body").empty();
+    $("body").css("animation-duration","0.5s");
+    $("body")[0].offsetHeight; // flushes CSS buffer
+    animateCss($("body"), "rotateOutDownRight", function () {
+        $("body").empty().attr("id",page+"Body");
 
         // console.log("Going to "+ page);
         $.get(page + ".html", function (data) {
 
-            $("body").append(data).css("overflow-x", "hidden");
-
-            animateCss($("body"), "slideInRight", function () {
-                $("body").css("overflow-x", "");
+            $("body").append(data);
+            animateCss($("body"), "rotateInDownRight", function () {
+                $("html").css("overflow", "");
+                $("body").css("animation-duration","");
+                $("body")[0].offsetHeight; // flushes CSS buffer
             });
         });
     });
+}
+
+function randomBackground(element, backgroundList, numberOfBackgrounds) {
+    var rand;
+    do {
+        rand = Math.floor((Math.random() * numberOfBackgrounds) + 1);
+    }
+    while (backgroundList[rand] === true);
+
+    element.css("background-image", "url(../images/" + $("body").attr("id").replace("Body", '') + "/backgrounds/" + rand +".png)");
+    backgroundList[rand] = true;
 }
