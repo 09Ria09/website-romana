350b63073f18cb35044e177fd009ac650a111b34
Rares Iordan <raresiordan09@gmail.com>
2019-02-23 18:43:40 +0200

    Now, the graph and the 'Personaje' parts are fully functional. TODO: Improve the design of the 'Personaje' part.

--- js/global.js
+++ js/global.js
@@ -11,20 +11,27 @@ function animateCss(element, animationName, callback) {
 }
 
 
-function changePage(page) {
+function changePage(page, direction = 'lr') {
+    let outAnimation = "rotateOutDownRight";
+    let inAnimation = "rotateInDownRight";
+    if (direction === 'rl') {
+        inAnimation = inAnimation.replace("Right", "Left");
+        outAnimation = outAnimation.replace("Right", "Left");
+    }
+
     $("html").css("overflow", "hidden");
     $("html")[0].offsetHeight; // flushes CSS buffer
 
     $("body").css("animation-duration", "0.5s");
     $("body")[0].offsetHeight; // flushes CSS buffer
-    animateCss($("body"), "rotateOutDownRight", function () {
+    animateCss($("body"), outAnimation, function () {
         $("body").empty().attr("id", page + "Body");
 
         // console.log("Going to "+ page);
         $.get(page + ".html", function (data) {
 
             $("body").append(data);
-            animateCss($("body"), "rotateInDownRight", function () {
+            animateCss($("body"), inAnimation, function () {
                 $("html").css("overflow", "");
                 $("body").css("animation-duration", "");
                 $("body")[0].offsetHeight; // flushes CSS buffer
@@ -45,83 +52,206 @@ function randomBackground(element, backgroundList, numberOfBackgrounds) {
     backgroundList[rand] = true;
 }
 
-const closedOpacity = 0.1;
-const hoverOpacity = 0.7;
-const openOpacity = 1;
+
+const hiddenOpacity = 0.1;
+const hoverOpacity = 0.8;
+const displayedOpacity = 1;
 
 const startTransDuration = "2s";
 const stateTransDuration = "0.5s";
 const hoverTransDuration = "0.3s";
 
-let state = "closed";
+let state = "hidden";
 let hovered = false;
 
+function addCuprinsSlider(backButton = false) {
 
-function addCuprinsSlider() {
+    if (backButton === false)
+        $("body").prepend('<div id="cuprinsSlideContainer"><div id="cuprins"></div><div id="slider">Cuprins</div></div>');
+    else {
+        $("body").prepend('<div id="cuprinsSlideContainer"><div id="cuprins"></div><div id="slider" style="display: flex">' +
+            '<div style="flex-basis: 95vh">Cuprins</div>' +
+            '<div id="back" style="flex-basis: 5vh; padding: 15px 0; border-top: 1px solid white">Inapoi</div>' +
+            '</div></div>');
+    }
 
-    $("body").prepend("<div id=\"cuprinsSlideContainer\"><div id=\"cuprins\"></div><div id=\"slider\">Cuprins</div></div>")
-	$.get("texts/" + $("body").attr("id").replace("Body", '') + "/cuprins.txt", function (data) {
-            let parsed = data.split(/\r?\n/);
-            // console.log(parsed);
-            $.each(parsed, function (outerN, data) {
-                let parsed = data.split("!link!");
-                $.each(parsed, function (n, data) {
-                    // console.log(parsed);
-                    if (n === 0)
-                        $("#cuprins").append('<a class="textCuprins" id="textCuprins' + outerN + '" style="margin: auto;">' + data + '</a>');
-                    else if (n === 1)
-                        $("#textCuprins" + outerN).attr("href", data);
-                });
-            });
+    $.getJSON("texts/" + $("body").attr("id").replace("Body", '') + "/cuprins.json", function (data) {
+        for (let i = 0; i < data.length; ++i) {
+            $("#cuprins").append('<a class="textCuprins" id="textCuprins' + i + '" href="' + data[i].href + '" style="margin: auto;">' + data[i].text + '</a>');
+        }
 
-            $("#cuprinsSlideContainer").addClass("noTransition").css("margin-left", '-' + $("#cuprins").outerWidth() + 'px');
-            $("#cuprinsSlideContainer")[0].offsetHeight; // flushes CSS buffer
-            $("#cuprinsSlideContainer").removeClass("noTransition");
-            $("#cuprinsSlideContainer")[0].offsetHeight; // flushes CSS buffer
+        $("#cuprinsSlideContainer").addClass("noTransition").css("margin-left", '-' + $("#cuprins").outerWidth() + 'px');
+        $("#cuprinsSlideContainer")[0].offsetHeight; // flushes CSS buffer
+        $("#cuprinsSlideContainer").removeClass("noTransition");
+        $("#cuprinsSlideContainer")[0].offsetHeight; // flushes CSS buffer
+
+    });
+
+    animateCss($("#cuprinsSlideContainer"), "bounceInLeft", function () {
+
+        setTimeout(function () {
+            if (state === "hidden" && hovered === false)
+                $("#cuprinsSlideContainer").css("transition-duration", startTransDuration).css("transition-timing-function", "ease").css("opacity", hiddenOpacity);
+        }, 1000);
+
+        $("#cuprinsSlideContainer").on("mouseover", function () {
+            hovered = true;
+            if (state === "hidden") {
+                $("#cuprinsSlideContainer").css("transition-duration", hoverTransDuration).css("transition-timing-function", "ease").css("opacity", hoverOpacity);
+            }
         });
-	animateCss($("#cuprinsSlideContainer"), "bounceInLeft", function () {
 
-            setTimeout(function () {
-                if (state === "closed" && hovered === false)
-                    $("#cuprinsSlideContainer").css("transition-duration", startTransDuration).css("transition-timing-function", "ease").css("opacity", closedOpacity);
-            }, 1000);
-
-            $("#cuprinsSlideContainer").on("mouseenter", function () {
-
-                if (state === "closed") {
-                    $("#cuprinsSlideContainer").css("transition-duration", hoverTransDuration).css("transition-timing-function", "ease").css("opacity", hoverOpacity);
-                }
-            });
-            $("#cuprinsSlideContainer").on("mouseover", function () {
-
-                hovered = true;
-            });
-            $("#cuprinsSlideContainer").on("mouseleave", function () {
-
-                hovered = false;
-                if (state === "closed") {
-                    $("#cuprinsSlideContainer").css("transition-duration", hoverTransDuration).css("transition-timing-function", "ease").css("opacity", closedOpacity);
-                }
-            });
-            $("#cuprinsSlideContainer").on("click", function (e) {
-
-                if ($(e.target).hasClass('textCuprins') === true)
-                    return;
-
-                if (state === "opened") {
-
-                    $("#cuprinsSlideContainer").css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
-                        .css("opacity", closedOpacity).css("margin-left", '-' + $("#cuprins").outerWidth() + 'px');
-
-                    state = "closed";
-                }
-                else {
-
-                    $("#cuprinsSlideContainer").css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
-                        .css("opacity", openOpacity).css("margin-left", "0");
-
-                    state = "opened";
-                }
-            });
+        $("#cuprinsSlideContainer").on("mouseleave", function () {
+            hovered = false;
+            if (state === "hidden") {
+                $("#cuprinsSlideContainer").css("transition-duration", hoverTransDuration).css("transition-timing-function", "ease").css("opacity", hiddenOpacity);
+            }
         });
+
+        $("#cuprinsSlideContainer").on("click", function (e) {
+            if ($(e.target).hasClass('textCuprins') === true)
+                return;
+            else if ($(e.target).attr('id') === 'back') {
+                changePage("fantastic", 'rl');
+                return;
+            }
+
+            if (state === "displayed") {
+
+                $("#cuprinsSlideContainer").css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
+                    .css("opacity", hiddenOpacity).css("margin-left", '-' + $("#cuprins").outerWidth() + 'px');
+
+                state = "hidden";
+            }
+            else {
+
+                $("#cuprinsSlideContainer").css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
+                    .css("opacity", displayedOpacity).css('margin-left', '0');
+
+                state = "displayed";
+            }
+        });
+    });
+}
+
+
+function addGraph(element) {
+
+    $("#"+element).append(
+        '<div id="graphCanvasContainer"></div><div id="nodeData"><div id="nodeDataText"></div></div>'
+    );
+
+    let option = {};
+    // create an array with nodes
+    let nodes = new vis.DataSet();
+    // create an array with edges
+    let edges = new vis.DataSet();
+    let currentlyReading;
+    let nodeId = 0;
+
+    let nodeDataDefault = "Apasa pe orice nod pentru a afla mai multe informatii.";
+    $("#nodeData").empty().append(nodeDataDefault);
+
+    // create a network
+    var container = document.getElementById("graphCanvasContainer");
+
+    var options = {
+        layout: {
+            hierarchical: {
+                enabled: true,
+                direction: 'UD',
+                nodeSpacing: 200
+            }
+        },
+        interaction: {
+            dragNodes: false,
+            dragView: false,
+            zoomView: false,
+        },
+        physics: {
+            enabled: false,
+            hierarchicalRepulsion: {
+                nodeDistance: 200
+            }
+        },
+        nodes: {
+            color: "red",
+            shape: "diamond",
+            size: 20,
+            font: {
+                face: "Century Gothic",
+                size: 16
+            }
+        },
+        edges: {
+            smooth: true,
+            arrows: {to: true}
+        }
+    };
+
+    // initialize your network!
+    $.getJSON("texts/" + $("body").attr("id").replace("Body", '') + "/" + element+".json", function (data) {
+        console.log(data);
+        // create an array with nodes
+        var nodes = new vis.DataSet(data.nodes);
+        // create an array with edges
+        var edges = new vis.DataSet(data.edges);
+        var dataFromDataSets = {
+            nodes: nodes,
+            edges: edges
+        };
+        var network = new vis.Network(container, dataFromDataSets, options);
+        network.on("selectNode", function (params) {
+            $("#nodeData").empty().append(data.nodeDataById[params.nodes[0]].data);
+        });
+        network.on("deselectNode", function (params) {
+            $("#nodeData").empty().append(nodeDataDefault);
+        });
+    });
+}
+
+
+function addPersonaje() {
+
+    $.getJSON("texts/" + $("body").attr("id").replace("Body", '') + "/personaje.json", function (data) {
+        for (let i = 0; i < data.length; ++i) {
+            $("#outerContainerPersonaje").append(
+                '<div id="containerPersonaj'+i+'" class="containerPersonaje">' +
+                '<div id    ="personaj' + i + '" class="personaje">' +
+                '<img src="images/'+ $("body").attr("id").replace("Body", '') + '/personaje/' + data[i].id + '.png">' +
+                '<h1>' +data[i].nume + '</h1>' +
+                '<p>' +data[i].descriereScurta + '</p>' +
+                '</div>' +
+                '<div id="extinderePersonaj'+i+'" class="extinderePersonaje">'+ data[i].descriereLunga +'</div>' +
+                '</div>'
+            );
+
+            let slideMargin="";
+            if (i % 2 === 0) {
+                    $("#personaj"+i).css("float", "left").css("padding-left", (parseInt($("#personaj" + i).css("padding-left").replace('px', '')) + 35) + 'px')
+                    .css("border-radius", "0 25px 25px 0");
+                    $("#extinderePersonaj"+i).css("float", "right").css("padding-right", "30vw").css("border-radius", "25px 0 0 25px");
+                    slideMargin="right";
+            }
+            else {
+                $("#personaj"+i).css("float", "right").css("border-radius", "25px 0 0 25px");
+                $("#extinderePersonaj"+i).css("float", "left").css("padding-left", "30vw")  //.css("padding-left", (parseInt($("#extinderePersonaj" + i).css("padding-left").replace('px', '')) + 35) + 'px')
+                    .css("border-radius", "0 25px 25px 0");
+                slideMargin="left";
+            }
+
+            $("#extinderePersonaj"+i).css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
+                .css("margin-"+slideMargin, '-' + $("#extinderePersonaj"+i).outerWidth() + 'px');
+
+            $("#personaj"+i).on("mouseenter", function () {
+                $("#extinderePersonaj"+i).css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
+                    .css("margin-"+slideMargin, '0');
+            });
+
+            $("#personaj"+i).on("mouseleave", function () {
+                $("#extinderePersonaj"+i).css("transition-duration", stateTransDuration).css("transition-timing-function", "ease")
+                    .css("margin-"+slideMargin, '-' + $("#extinderePersonaj"+i).outerWidth() + 'px');
+            });
+        }
+    });
 }
